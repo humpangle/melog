f825fd6d24eebf2e98605e0179c5786c304648ec
Kanmii <maneptha@gmail.com>
2018-01-14 08:20:58 +0100

    Use advanced graphiql interface in development.

--- server/lib/melog_web/router.ex
+++ server/lib/melog_web/router.ex
@@ -2,25 +2,36 @@ defmodule MelogWeb.Router do
   use MelogWeb, :router
 
   pipeline :api do
-    plug :accepts, ["json"]
-    plug Guardian.Plug.Pipeline,
+    plug(:accepts, ["json"])
+
+    plug(
+      Guardian.Plug.Pipeline,
       module: MelogWeb.UserSerializer,
       error_handler: MelogWeb.UserSerializer
-    plug Guardian.Plug.VerifyHeader, realm: "Bearer"
-    plug Guardian.Plug.LoadResource, allow_blank: true
-    plug MelogWeb.Context
+    )
+
+    plug(Guardian.Plug.VerifyHeader, realm: "Bearer")
+    plug(Guardian.Plug.LoadResource, allow_blank: true)
+    plug(MelogWeb.Context)
   end
 
   scope "/" do
-    pipe_through :api
+    pipe_through(:api)
 
-    forward "/api", Absinthe.Plug,
+    forward(
+      "/api",
+      Absinthe.Plug,
       schema: MelogWeb.Schema,
       context: %{pubsub: MelogWeb.Endpoint}
+    )
 
-    forward "/graphiql", Absinthe.Plug.GraphiQL,
-      schema: MelogWeb.Schema,
-      interface: :simple,
-      context: %{pubsub: MelogWeb.Endpoint}
+    if Mix.env() == :dev do
+      forward(
+        "/graphiql",
+        Absinthe.Plug.GraphiQL,
+        schema: MelogWeb.Schema,
+        context: %{pubsub: MelogWeb.Endpoint}
+      )
+    end
   end
 end
