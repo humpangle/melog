c1c652e4265745c8c7ecb0d8cfa494948d7a8f22
Kanmii <maneptha@gmail.com>
2018-01-21 08:28:01 +0100

    Use elixir version 1.6.0.

--- server/mix.exs
+++ server/mix.exs
@@ -5,7 +5,7 @@ defmodule Melog.Mixfile do
     [
       app: :melog,
       version: "0.0.1",
-      elixir: "~> 1.6.0-rc.1",
+      elixir: "~> 1.6.0",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
